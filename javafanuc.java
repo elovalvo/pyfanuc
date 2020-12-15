c62eff9ffcc6fb2c362c5d9bb90b36062a7ba3af
Dennis Heynlein <clogic@gmx.de>
2020-12-15 20:48:49 +0100

    Update javafanuc.java

--- javafanuc.java
+++ javafanuc.java
@@ -58,4 +58,41 @@ public class Main {
         b.get(ret);
         return ret;
     }
+    
+    static byte[][] decap(byte[] packetdata) throws IllegalAccessException{
+        if(packetdata.length>=10) {
+            if(packetdata[0] == (byte)0xa0 && packetdata[1] == (byte)0xa0 && packetdata[2] == (byte)0xa0 && packetdata[3] == (byte)0xa0) {
+                int fvers = packetdata[4] << 8 | packetdata[5];
+                int ftype = packetdata[6] << 8 | packetdata[7];
+                int len1 = packetdata[8] << 8 | packetdata[9];
+                if(len1 + 10 == packetdata.length) {
+                    if(len1==0) return null;
+                    if(ftype==0x2102) { //Response
+                        int qu=packetdata[10] << 8 | packetdata[11];
+                        if(qu>((packetdata.length-12)/2))
+                            throw new IllegalAccessException("PROTOCOL decode error");
+                        byte[][] re=new byte[qu][];
+                        int count=0;
+                        int n=12;
+                        try {
+                            while (n < packetdata.length) {
+                                int le = packetdata[n] << 8 | packetdata[n + 1];
+                                byte[] data = new byte[le - 2];
+                                n += 2;
+                                for (int n2 = 0; n2 < (le - 2); n2++)
+                                    data[n2] = packetdata[n++];
+                                re[count++] = data;
+                            }
+                        }
+                        catch(ArrayIndexOutOfBoundsException e) {
+                            throw new IllegalAccessException("PROTOCOL decode error");
+                        }
+                        if(count != re.length) throw new IllegalAccessException("PROTOCOL decode error");
+                        return re;
+                    }
+                }
+            }
+        }
+        throw new IllegalAccessException("PROTOCOL decode error");
+    }
 }
